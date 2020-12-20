a333abbd06559888ff1117e4d1b29df605753d5e
rayzxu <ray.z.xu@outlook.com>
2020-12-20 22:59:12 +0800

    fixed: LIST缺失 return

--- Week_13/carousel.js
+++ Week_13/carousel.js
@@ -1,91 +1,126 @@
-import {Component} from './framework.js'
+import {Component, STATE, ATTRIBUTE} from './framework.js'
+import { enableGesture } from './gesture.js'
+import { Timeline, Animation } from './animation.js'
+import { ease, easeIn, easeInOut, easeOut } from './ease';
+
+export { STATE, ATTRIBUTE } from './framework.js'
 
 export class Carousel extends Component {
     constructor() {
         super();
-        this.attributes = Object.create(null);
+        
         // this.root = document.createElement("div")
     }
-    setAttribute(name, value) {
-        this.attributes[name] = value
-    }
+    
     render() {
-        console.log(this.attributes.src)
         this.root = document.createElement('div')
         this.root.classList.add("carousel")
 
-        for(let record of this.attributes.src) {
+        for(let record of this[ATTRIBUTE].src) {
             let child = document.createElement('div')
-            child.style.backgroundImage = `url(${record})`;
+            child.style.backgroundImage = `url(${record.img})`;
             // child.style.display = 'none';
             this.root.appendChild(child)
         }
+        enableGesture(this.root)
+        let timeLine = new Timeline()
+        timeLine.start();
 
-        let position = 0;
+        let children = this.root.children;
 
-        this.root.addEventListener('mousedown', event => {
-            let children = this.root.children;
-            let startX = event.clientX
-            let startY = event.clientY
-            let move = event => {
-                let x = event.clientX - startX
+        // let position = 0;
+        this[STATE].position = 0
+        
+        let t = 0; // 动画时间
+        let ax = 0;
+        let handler = null;
 
-                let current = position - Math.round(( x - x % 500) / 500);
-
-                for (let offset of [-1, 0, 1]) {
-                    let pos = current + offset;
-                    pos = (pos + children.length) % children.length
-
-                    /* if (offset === 0) {
-                        position = pos;
-                    } */
-
-                    children[pos].style.transition = 'none'
-                    children[pos].style.transform = `translateX(${- pos * 500 + offset * 500 + x % 500}px)`
-                }
+        this.root.addEventListener("start", event => {
+            timeLine.pause()
+            clearInterval(handler)
+            if(Date.now() - t < 1500) {
+                let progress = (Date.now() - t) / 1500;
+                ax = ease(progress) * 500 - 500;
+            } else {
+                ax = 0
             }
-
-            let up = event => {
-                console.log('up')
-                let x = event.clientX - startX
-                position = position - Math.round(x / 500)
-                for (let offset of [0, - Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
-                    let pos = position + offset;
-                    pos = (pos + children.length) % children.length
-
-                    children[pos].style.transition = ''
-                    children[pos].style.transform = `translateX(${- pos * 500 + offset * 500}px)`
-                }
-                document.removeEventListener("mousemove", move)
-                document.removeEventListener("mouseup", up)
-            }
-
-            document.addEventListener("mousemove", move)
-            document.addEventListener("mouseup", up)
         })
-        /* let currentIndex = 0
-        setInterval(() => {
-            let children = this.root.children;
-            let nextIndex = ( currentIndex + 1 ) % children.length;
+        this.root.addEventListener("tap", event => {
+            this.triggerEvent("click", {
+                data: this[ATTRIBUTE].src[this[STATE].position],
+                position: this[STATE].position
+            })
+        })
 
-            let current = children[currentIndex];
+        this.root.addEventListener("pan", event => {
+            let x = event.clientX - event.startX - ax;
+            let current = this[STATE].position - Math.round(( x - x % 500) / 500);
+
+            console.log(event.clientX - event.startX)
+            for (let offset of [-1, 0, 1]) {
+                let pos = current + offset;
+                pos = (pos % children.length + children.length) % children.length
+
+                children[pos].style.transition = 'none'
+                children[pos].style.transform = `translateX(${- pos * 500 + offset * 500 + x % 500}px)`
+            }
+        })
+
+        this.root.addEventListener("end", event => {
+            timeLine.reset()
+            timeLine.start()
+            handler = setInterval(nextPicture, 3000)
+
+            let x = event.clientX - event.startX - ax;
+            let current = this[STATE].position - (( x - x % 500) / 500);
+
+            let direction = Math.round((x % 500) / 500)
+
+            if(event.isFlick) {
+                if(x < 0) {
+                    direction = Math.ceil((x % 500) / 500)
+                } else {
+                    direction = Math.floor((x % 500) / 500)
+                }
+            }
+
+            console.log(event.clientX - event.startX)
+            for (let offset of [-1, 0, 1]) {
+                let pos = current + offset;
+                pos = (pos % children.length + children.length) % children.length
+
+                children[pos].style.transition = 'none'
+                timeLine.add(new Animation(children[pos].style, "transform", 
+                - pos * 500 + offset * 500 + x % 500,
+                - pos * 500 + offset * 500 + direction * 500,
+                500, 0, ease, v => `translateX(${v}px)`))
+            }
+
+            this[STATE].position = this[STATE].position - (( x - x % 500) / 500) - direction;
+            this[STATE].position = (this[STATE].position % children.length + children.length) % children.length
+            this.triggerEvent("Change", {position: this[STATE].position})
+        })
+
+        let nextPicture = () => {
+            let children = this.root.children;
+            let nextIndex = ( this[STATE].position + 1 ) % children.length;
+
+            let current = children[this[STATE].position];
             let next = children[nextIndex]
 
-            next.style.transition = 'none';
-            next.style.transform = `translateX(${100 - nextIndex * 100}%)`
+            t = Date.now()
 
-            setTimeout(() => {
-                next.style.transition = "",
-                current.style.transform = `translateX(${-100 - currentIndex * 100}%)`
-                next.style.transform = `translateX(${- nextIndex * 100}%)`
+            timeLine.add(new Animation(current.style, "transform", - this[STATE].position * 500, - 500 - this[STATE].position * 500, 500, 0, ease, v => `translateX(${v}px)`))
 
-                currentIndex = nextIndex;
-            }, 16)
-        }, 3000) */
+            timeLine.add(new Animation(next.style, "transform", 500 - nextIndex * 500, - nextIndex * 500, 500, 0, ease, v => `translateX(${v}px)`))
+            
+            this[STATE].position = nextIndex;
+            this.triggerEvent("Change", {position: this[STATE].position})
+        }
+
+        handler = setInterval(nextPicture, 3000)
 
         return this.root
     }
-    mountTo(parent) {
-        parent.appendChild(this.render())
-    }
+    
 }
--- Week_13/main.js
+++ Week_13/main.js
@@ -1,6 +1,8 @@
 import {Component, createElement} from './framework.js'
-import { Carousel } from './carousel'
-import {Timeline, Animation} from './animation'
+import { Carousel } from './Carousel.js'
+import { Button } from './Button.js'
+import { List } from './List.js'
+/* import {Timeline, Animation} from './animation'
 
 let tl = new Timeline()
 
@@ -8,14 +10,45 @@ window.animation = new Animation({ set a(v) {console.log(v)}}, "a", 0, 100, 1000
 window.tl = tl
 
 // tl.add(new Animation({ set a(v) {console.log(v)}}, "a", 0, 100, 1000, null))
-tl.start()
 
-/* let d = [
-    './1.jpeg',
-    './2.jpeg',
-    './3.jpg',
-    './4.jpeg'
+let a = <Carousel src={d} 
+    onChange={event => console.log(event.detail.position)}
+    onClick={event => window.location.href = event.detail.data.url}></Carousel> */
+
+/* let a = <Button>
+    content
+</Button> */
+let d = [
+    {
+        img: './1.jpeg',
+        url: 'https://www.baidu.com',
+        title: '1'
+    },
+    {
+        img: './2.jpeg',
+        url: 'http://www.mhhf.com/games',
+        title: '2'
+    },
+    {
+        img: './3.jpg',
+        url: 'https://www.cnblogs.com/wenxuehai/p/10416862.html',
+        title: '3'
+    },
+    {
+        img: './4.jpeg',
+        url: 'https://www.baidu.com',
+        title: '4'
+    }
 ]
-
-let a = <Carousel src={d}></Carousel>
-a.mountTo(document.body) */
+let a = <List data={d}>
+    {
+        (record) => {
+            return <div>
+                <img src={record.img}/>
+                <a href={record.url}>{record.title}</a>
+            </div>
+        }
+    }
+</List>
+console.log('a', a)
+a.mountTo(document.body)
